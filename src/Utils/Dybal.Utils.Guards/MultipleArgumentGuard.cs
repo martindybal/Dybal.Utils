d6c67a425ab5edbc809b56ed48dd959bb84af211
Martin Dybal <martin@dybal.it>
2022-10-15 11:02:41 +0200

    Add If

--- src/Utils/Dybal.Utils.Guards/MultipleArgumentGuard.cs
+++ src/Utils/Dybal.Utils.Guards/MultipleArgumentGuard.cs
@@ -3,11 +3,20 @@
 public class MultipleArgumentGuard
 {
     public Argument<object?>[] Arguments { get; }
-    public bool IsActive { get; }
+    public bool IsActive { get; private set; }
 
     internal MultipleArgumentGuard(Argument<object?>[] arguments, bool isActive)
     {
         Arguments = arguments;
         IsActive = isActive;
+    }
+
+    public MultipleArgumentGuard If(bool condition)
+    {
+        if (!condition)
+        {
+            IsActive = false;
+        }
+        return this;
     }
 }
