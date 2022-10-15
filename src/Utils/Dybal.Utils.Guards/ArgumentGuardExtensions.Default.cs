baed77aaf25bf41526aba7738078c73b57ab8686
Martin Dybal <martin@dybal.it>
2022-10-15 15:03:19 +0200

    + Default

---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.Default.cs
@@ -0,0 +1,21 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static TArgument? Default<TArgument>(this IArgumentGuard<TArgument> guard, string? message = null)
+        where TArgument : struct
+    {
+        if (guard.IsActive)
+        {
+            if (!guard.ArgumentValue.Equals(default(TArgument)))
+            {
+                var defaultMessage = guard.ArgumentValue is Guid ? 
+                                        "Value must be an empty GUID." : 
+                                        "Value must be a default value.";
+                throw new ArgumentException(message ?? defaultMessage, guard.ArgumentName);
+            }
+        }
+
+        return guard.ArgumentValue;
+    }
+}
