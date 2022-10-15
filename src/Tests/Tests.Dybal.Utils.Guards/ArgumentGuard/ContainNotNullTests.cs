8436eaf823c85e20bf8918110efba14fc070bfe8
Martin Dybal <martin@dybal.it>
2022-10-15 14:15:49 +0200

    + ContainNotNull

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/ContainNotNullTests.cs
@@ -0,0 +1,52 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class ContainNotNullTests : UnitTestsBase
+{
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_CollectionEmpty()
+    {
+        // Arrange
+        var source = Array.Empty<int>();
+
+        void Act()
+        {
+            Guard.Argument(source).ContainNotNull();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Collection has to contain an item with not default value. (Parameter 'source')", ex.Message);
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_CollectionContainNotNull()
+    {
+        // Arrange
+        var source = new int?[] { 1, null, null };
+
+        // Act
+        Guard.Argument(source).ContainNotNull();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_CollectionContainOnlyNull()
+    {
+        // Arrange
+        var source = new int?[] { null };
+
+        void Act()
+        {
+            Guard.Argument(source).ContainNotNull();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Collection has to contain an item with not default value. (Parameter 'source')", ex.Message);
+    }
+}
---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.ContainNotNull.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static IEnumerable<TArgument> ContainNotNull<TArgument>(this IArgumentGuard<IEnumerable<TArgument>> guard, string? message = null)
+    {
+        return guard.Contain(item => item is not null, message ?? "Collection has to contain an item with not default value.");
+    }
+}
