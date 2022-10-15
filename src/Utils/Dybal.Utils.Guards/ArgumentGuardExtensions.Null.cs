3636258367e6fed02e2d45f1a5e12d9721d847d6
Martin Dybal <martin@dybal.it>
2022-10-15 13:42:30 +0200

    Remove Argument<TArgument> from Argument guard - less allocation - covariance

--- src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.Null.cs
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.Null.cs
@@ -4,11 +4,11 @@ public static partial class ArgumentGuardExtensions
 {
     public static TArgument? Null<TArgument>(this ArgumentGuard<TArgument> guard, string? message = null)
     {
-        if (guard.IsActive && guard.Argument.Value is not null)
+        if (guard.IsActive && guard.ArgumentValue is not null)
         {
-            throw new ArgumentException(message ?? "Value must be null.", guard.Argument.Name);
+            throw new ArgumentException(message ?? "Value must be null.", guard.ArgumentName);
         }
 
-        return guard.Argument.Value;
+        return guard.ArgumentValue;
     }
 }
