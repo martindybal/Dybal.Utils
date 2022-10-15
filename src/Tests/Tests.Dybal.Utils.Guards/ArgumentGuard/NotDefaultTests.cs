3250eb19252cc3baa155bfc47db234d2e7eda13f
Martin Dybal <martin@dybal.it>
2022-10-15 15:03:24 +0200

    + NotDefault

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/NotDefaultTests.cs
@@ -0,0 +1,213 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class NotDefaultTests : UnitTestsBase
+{
+    [Fact]
+    public void Should_NotThrows_When_VariableWithValue()
+    {
+        // Arrange
+        var value = Guid.NewGuid();
+
+        // Act
+        Guard.Argument(value).NotDefault();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_NullableVariableWithValue()
+    {
+        // Arrange
+        Guid? value = Guid.NewGuid();
+
+        // Act
+        Guard.Argument(value).NotDefault();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_PropertyWithValue()
+    {
+        // Arrange
+        var sample = new { Value = Guid.NewGuid() };
+
+        // Act
+        Guard.Argument(sample.Value).NotDefault();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_GuidVariableHasDefaultValue()
+    {
+        // Arrange
+        var value = Guid.Empty;
+
+        void Act()
+        {
+            Guard.Argument(value).NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be an empty GUID. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_NullableGuidVariableNull()
+    {
+        // Arrange
+        Guid? value = null;
+
+        void Act()
+        {
+            Guard.Argument(value).NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Nullable object must have a value. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void IfHasValue_ShouldThrows_ArgumentException_When_NullableGuidVariableDefault()
+    {
+        // Arrange
+        Guid? value = Guid.Empty;
+
+        void Act()
+        {
+            var id = Guard.Argument(value).IfHasValue().NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be an empty GUID. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void IfHasValue_ShouldNotThrows_ArgumentException_When_NullableGuidNull()
+    {
+        // Arrange
+        Guid? value = null;
+
+        // Act
+        var id = Guard.Argument(value).IfHasValue().NotDefault() ?? Guid.NewGuid();
+        
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_VariableHasDefaultValue()
+    {
+        // Arrange
+        var value = 0;
+
+        void Act()
+        {
+            Guard.Argument(value).NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be the default value. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_GuidPropertyHasDefaultValue()
+    {
+        // Arrange
+        var sample = new { Value = Guid.Empty };
+
+        void Act()
+        {
+            Guard.Argument(sample.Value).NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be an empty GUID. (Parameter 'sample.Value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_PropertyHasDefaultValue()
+    {
+        // Arrange
+        var sample = new { Value = 0 };
+
+        void Act()
+        {
+            Guard.Argument(sample.Value).NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be the default value. (Parameter 'sample.Value')", ex.Message);
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_ReferenceVariableWithValue()
+    {
+        // Arrange
+        var value = new object();
+
+        // Act
+        Guard.Argument(value).NotDefault();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_ReferencePropertyWithValue()
+    {
+        // Arrange
+        var sample = new { Value = new object() };
+
+        // Act
+        Guard.Argument(sample.Value).NotDefault();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_VariableHasNullVariable()
+    {
+        // Arrange
+        var value = (object)null!;
+
+        void Act()
+        {
+            Guard.Argument(value).NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be the default value. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_PropertyHasNullValue()
+    {
+        // Arrange
+        var sample = new { Value = (object)null! };
+
+        void Act()
+        {
+            Guard.Argument(sample.Value).NotDefault();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be the default value. (Parameter 'sample.Value')", ex.Message);
+    }
+}
---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensionsNotDefault.cs
@@ -0,0 +1,36 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static TArgument NotDefault<TArgument>(this IArgumentGuard<TArgument> guard, string? message = null)
+    {
+        if (guard.IsActive)
+        {
+            if (EqualityComparer<TArgument>.Default.Equals(guard.ArgumentValue, default))
+            {
+                var defaultMessage = guard.ArgumentValue is Guid ?
+                                        "Value cannot be an empty GUID." :
+                                        "Value cannot be the default value.";
+                throw new ArgumentException(message ?? defaultMessage, guard.ArgumentName);
+            }
+        }
+
+        return guard.ArgumentValue;
+    }
+
+    public static TArgument? NotDefault<TArgument>(this IArgumentGuard<TArgument?> guard, string? message = null)
+        where TArgument : struct
+    {
+        if (guard.IsActive)
+        {
+            if (guard.ArgumentValue.HasValue)
+            {
+                return Guard.Argument(guard.ArgumentValue.Value, guard.ArgumentName).NotDefault();
+            }
+
+            throw new ArgumentException(message ?? "Nullable object must have a value.", guard.ArgumentName);
+        }
+
+        return guard.ArgumentValue;
+    }
+}
