3fa6f234cbc22e234527ed1b1a3c2607f5b2717d
Martin Dybal <martin@dybal.it>
2022-10-19 08:39:22 +0200

    Simplify ThrowHelper

--- src/Utils/Dybal.Utils.Guards/ThrowHelper.cs
+++ src/Utils/Dybal.Utils.Guards/ThrowHelper.cs
@@ -1,27 +1,20 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Dybal.Utils.Guards
 {
     internal static class ThrowHelper
     {
         [DoesNotReturn]
-        public static void ThrowArgumentNullException(string paramName)
+        public static void ThrowArgumentNullException(string paramName, string? message = null)
         {
-            throw new ArgumentNullException(paramName);
-        }
-
-        [DoesNotReturn]
-        public static void ThrowArgumentNullException(string paramName, string message)
-        {
+            if (message is null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
             throw new ArgumentNullException(paramName, message);
         }
-
-        [DoesNotReturn]
-        public static void ThrowArgumentException(string message)
-        {
-            throw new ArgumentException(message);
-        }
-
+        
         [DoesNotReturn]
         public static void ThrowArgumentException(string message, string paramName)
         {
