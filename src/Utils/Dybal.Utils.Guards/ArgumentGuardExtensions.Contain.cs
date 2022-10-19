f328f075140afc5fb11fd1d189a8db834ead6c1c
Martin Dybal <martin@dybal.it>
2022-10-19 16:52:38 +0200

    Allow own IEnumerable type

--- src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.Contain.cs
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.Contain.cs
@@ -2,16 +2,25 @@
 
 public static partial class ArgumentGuardExtensions
 {
-    public static IEnumerable<TArgument> Contain<TArgument>(this IArgumentGuard<IEnumerable<TArgument>> guard, TArgument? value, string? message = null)
+    public static TEnumerable Contain<TEnumerable, TArgument>(this IArgumentGuard<TEnumerable> guard, TArgument? value, string? message = null)
+        where TEnumerable : IEnumerable<TArgument>
     {
-        return guard.Contain(item => Equals(item, value), message ?? $"Collection has to contain '{value}'.");
+        message ??= $"Collection has to contain '{value}'.";
+        return guard.Contain<TEnumerable, TArgument>(item => Equals(item, value), message);
+    }
+    
+    public static IEnumerable<TArgument> Contain<TArgument>(this IArgumentGuard<IEnumerable<TArgument>> guard, Func<TArgument, bool> filter, string? message = null)
+    {
+        return guard.Contain<IEnumerable<TArgument>, TArgument>(filter, message);
     }
 
-    public static IEnumerable<TArgument> Contain<TArgument>(this IArgumentGuard<IEnumerable<TArgument>> guard, Func<TArgument, bool> filter, string? message = null)
+    public static TEnumerable Contain<TEnumerable, TArgument>(this IArgumentGuard<TEnumerable> guard, Func<TArgument, bool> filter, string? message = null)
+        where TEnumerable : IEnumerable<TArgument>
     {
         if (!guard.Argument.Value.Any(filter))
         {
-            ThrowHelper.Throw<ArgumentException>(guard, message ?? "Collection does not contain required item.");
+            message ??= "Collection does not contain required item.";
+            ThrowHelper.Throw<ArgumentException>(guard, message);
         }
 
         return guard.Argument.Value;
--- src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.ContainNotNull.cs
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.ContainNotNull.cs
@@ -4,6 +4,12 @@ public static partial class ArgumentGuardExtensions
 {
     public static IEnumerable<TArgument> ContainNotNull<TArgument>(this IArgumentGuard<IEnumerable<TArgument>> guard, string? message = null)
     {
-        return guard.Contain(static item => item is not null, message ?? "Collection has to contain an item with not default value.");
+        return guard.ContainNotNull<IEnumerable<TArgument>, TArgument>(message);
+    }
+
+    public static TEnumerable ContainNotNull<TEnumerable, TArgument>(this IArgumentGuard<TEnumerable> guard, string? message = null)
+        where TEnumerable : IEnumerable<TArgument>
+    {
+        return guard.Contain<TEnumerable, TArgument>(static item => item is not null, message ?? "Collection has to contain an item with not default value.");
     }
 }
