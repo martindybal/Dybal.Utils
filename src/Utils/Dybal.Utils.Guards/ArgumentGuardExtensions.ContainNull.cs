1712a0537759bcefa842866c8531622dc4679cbd
Martin Dybal <martin@dybal.it>
2022-10-19 16:52:38 +0200

    Allow own IEnumerable type

--- src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.ContainNull.cs
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.ContainNull.cs
@@ -4,6 +4,12 @@ public static partial class ArgumentGuardExtensions
 {
     public static IEnumerable<TArgument> ContainNull<TArgument>(this IArgumentGuard<IEnumerable<TArgument>> guard, string? message = null)
     {
-        return guard.Contain(static item => item is null, message ?? "Collection has to contain null.");
+        return guard.ContainNull<IEnumerable<TArgument>, TArgument>();
+    }
+
+    public static TEnumerable ContainNull<TEnumerable, TArgument>(this IArgumentGuard<TEnumerable> guard, string? message = null)
+        where TEnumerable : IEnumerable<TArgument>
+    {
+        return guard.Contain<TEnumerable, TArgument>(static item => item is null, message ?? "Collection has to contain null.");
     }
 }
