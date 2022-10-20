75c57646f47c5cbe00e5497a3f431c5883a87ee3
Martin Dybal <martin@dybal.it>
2022-10-20 08:46:29 +0200

    Add tests for CompactList

---
+++ src/Tests/Tests.Dybal.Utils.Guards/CompactList/CountTests.cs
@@ -0,0 +1,67 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards;
+
+public class CountTests : UnitTestsBase
+{
+    [Fact]
+    public void ShouldReturn_2_When_Contains2Elements()
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 2);
+        
+        // Assert
+        Assert.Equal(2, compactList.Count);
+    }
+
+    [Fact]
+    public void ShouldReturn_3_When_Contains3Elements()
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 2, 3);
+        
+        // Assert
+        Assert.Equal(3, compactList.Count);
+    }
+
+    [Fact]
+    public void ShouldReturn_4_When_Contains4Elements()
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 2, 3, 4);
+        
+        // Assert
+        Assert.Equal(4, compactList.Count);
+    }
+
+    [Fact]
+    public void ShouldReturn_5_When_Contains5Elements()
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 2, 3, 4, 5);
+        
+        // Assert
+        Assert.Equal(5, compactList.Count);
+    }
+
+    [Fact]
+    public void ShouldReturn_6_When_Contains6Elements()
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 2, 3, 4, 5, 6);
+        
+        // Assert
+        Assert.Equal(6, compactList.Count);
+    }
+
+    [Fact]
+    public void ShouldReturn_10_When_Contains10Elements()
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        
+        // Assert
+        Assert.Equal(10, compactList.Count);
+    }
+}
---
+++ src/Tests/Tests.Dybal.Utils.Guards/CompactList/IndexerTests.cs
@@ -0,0 +1,51 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards;
+
+public class IndexerTests : UnitTestsBase
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    public void ShouldReturn_ItemOnIndex_WhenIndexInRange(int index)
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Act
+        var actualItem = compactList[index];
+
+        // Assert
+        Assert.Equal(index + 1, actualItem);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(10)]
+    public void ShouldThrow_ItemOnIndex_WhenIndexOutOfRange(int indexOutOfRange)
+    {
+        // Arrange
+        var compactList = new CompactList<int>(1, 1, 1);
+
+        // Act
+        void Act()
+        {
+            var actualItem = compactList[indexOutOfRange];
+        }
+
+        // Assert
+        var argumentOutOfRangeException = Assert.Throws<ArgumentOutOfRangeException>(Act);
+        Assert.Equal("Provided index was outside of the valid range. (Parameter 'index')", argumentOutOfRangeException.Message);
+    }
+}
