1a20f8a583cb6587632e9cc351cfb4eea6f9d4e4
Martin Dybal <martin@dybal.it>
2022-10-20 08:46:29 +0200

    Add tests for CompactList

---
+++ src/Tests/Tests.Dybal.Utils.Guards/CompactList/GetEnumerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards;
+
+public class GetEnumerator : UnitTestsBase
+{
+    [Fact]
+    public void Generic_ShouldReturn_SequenceWithAllElements()
+    {
+        var expectedSequence = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var compactList = new CompactList<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Act
+        var actualSequence = new List<int>();
+        using var enumerator = compactList.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            actualSequence.Add(enumerator.Current);
+        }
+        
+        Assert.Equal(actualSequence, expectedSequence);
+    }
+
+    [Fact]
+    public void NonGeneric_ShouldReturn_SequenceWithAllElements()
+    {
+        var expectedSequence = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        IEnumerable compactList = new CompactList<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Act
+        var actualSequence = new List<int>();
+        var enumerator = compactList.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            actualSequence.Add((int)enumerator.Current);
+        }
+        
+        Assert.Equal(actualSequence, expectedSequence);
+    }
+}
