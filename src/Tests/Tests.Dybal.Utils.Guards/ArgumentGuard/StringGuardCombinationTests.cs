a2964abe156098bd5c96e21538dd7d7d7f2f43b8
Martin Dybal <martin@dybal.it>
2022-10-25 10:43:06 +0200

    Sample of use multiple guards

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/StringGuardCombinationTests.cs
@@ -0,0 +1,20 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class StringGuardCombinationTests : UnitTestsBase
+{
+    [Fact]
+    public void Should_NotThrows_When_AllGuardsIsOk()
+    {
+        // Arrange
+        string value = "abc";
+
+        // Act
+        string guardValue = Guard.Argument(value).NotNullOrWhiteSpace().MinLength(2).MaxLength(5);
+
+        // Assert
+        Assert.Equal(value, guardValue);
+    }
+}
