196dd5e594071ceacae57ca44fc79bc33fe23b59
Martin Dybal <martin@dybal.it>
2022-10-25 14:53:49 +0200

    Add Empty

---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.Empty.cs
@@ -0,0 +1,22 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<IEnumerable<TArgument>> Empty<TArgument>(this ICovariantArgumentGuard<IEnumerable<TArgument>> guard, string? message = null)
+    {
+        return guard.Empty<IEnumerable<TArgument>, TArgument>(message);
+    }
+
+    public static ArgumentGuard<TEnumerable> Empty<TEnumerable, TArgument>(this ICovariantArgumentGuard<TEnumerable> covariantGuard, string? message = null)
+        where TEnumerable : IEnumerable<TArgument>
+    {
+        var guard = ArgumentGuard<TEnumerable>.From(covariantGuard, covariantGuard.Argument);
+        if (guard.Argument.Value.Any())
+        {
+            message ??= "Collection has to be empty.";
+            ThrowHelper.Throw<ArgumentException>(guard, message);
+        }
+
+        return guard;
+    }
+}
