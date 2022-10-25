2bc61ad50b407746a263db0d4b6e71d001849412
Martin Dybal <martin@dybal.it>
2022-10-25 09:41:41 +0200

    Add MaxLength

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/MaxLengthTests.cs
@@ -0,0 +1,101 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class MaxLengthTests : UnitTestsBase
+{
+    [Fact]
+    public void Should_NotThrows_When_ValueHasFewerCharacters()
+    {
+        // Arrange
+        string value = "ab";
+
+        // Act
+        Guard.Argument(value).MaxLength(3);
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_ValueHasMaximumCharacters()
+    {
+        // Arrange
+        string value = "abc";
+
+        // Act
+        Guard.Argument(value).MaxLength(3);
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_ValueHasMoreCharacters()
+    {
+        // Arrange
+        string value = "abcD";
+
+        // Act
+
+        void Act()
+        {
+            Guard.Argument(value).MaxLength(3);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("The length of 'value' must be 3 characters or fewer. Parameter 4 has characters. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_WithCustomMessage_When_ValueHasMoreCharactersAndCustomMessageWasUsed()
+    {
+        // Arrange
+        string value = "abcD";
+        var customMessage = "Custom message.";
+
+        // Act
+
+        void Act()
+        {
+            value = Guard.Argument(value).MaxLength(3, customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal($"{customMessage} (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_CustomException_WithCustomMessage_When_ValueHasMoreCharactersAndCustomExceptionAndMessageWasUsed()
+    {
+        // Arrange
+        string value = "abcD";
+        var customMessage = "Custom message.";
+
+        // Act
+
+        void Act()
+        {
+            ThrowHelper.Register((paramName, message) => new CustomException(paramName, message));
+            value = Guard.Argument(value).Throws<CustomException>().MaxLength(3, customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<CustomException>(Act);
+        Assert.Equal(customMessage, ex.Message);
+    }
+
+    class CustomException : Exception
+    {
+        public string ParamName { get; }
+
+        public CustomException(string paramName, string? message)
+            : base(message)
+        {
+            ParamName = paramName;
+        }
+    }
+}
---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.MaxLength.cs
@@ -0,0 +1,14 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<string> MaxLength(this ArgumentGuard<string> guard, uint maxLength, string? message = null)
+    {
+        if (guard.Argument.Value.Length > maxLength)
+        {
+            ThrowHelper.Throw<ArgumentException>(guard, message ?? $"The length of '{guard.Argument.Name}' must be {maxLength} characters or fewer. Parameter {guard.Argument.Value.Length} has characters.");
+        }
+
+        return guard;
+    }
+}
