991fdc4da564ab9e2359ab7ade5fd227ef127dbd
Martin Dybal <martin@dybal.it>
2022-10-25 09:41:43 +0200

    Add MinLength

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/MinLengthTests.cs
@@ -0,0 +1,101 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class MinLengthTests : UnitTestsBase
+{
+    [Fact]
+    public void Should_NotThrows_When_ValueHasMoreCharacters()
+    {
+        // Arrange
+        string value = "abc";
+
+        // Act
+        string guardValue = Guard.Argument(value).MinLength(2);
+
+        // Assert
+        Assert.Equal(value, guardValue);
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_ValueHasMinimumCharacters()
+    {
+        // Arrange
+        string value = "abc";
+
+        // Act
+        string guardValue = Guard.Argument(value).MinLength(3);
+
+        // Assert
+        Assert.Equal(value, guardValue);
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_ValueHasFewerCharacters()
+    {
+        // Arrange
+        string value = "ab";
+
+        // Act
+
+        void Act()
+        {
+            value = Guard.Argument(value).MinLength(3);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("The length of 'value' must be 3 characters or more. Parameter 2 has characters. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_WithCustomMessage_When_ValueHasFewerCharactersAndCustomMessageWasUsed()
+    {
+        // Arrange
+        string value = "ab";
+        var customMessage = "Custom message.";
+
+        // Act
+
+        void Act()
+        {
+            value = Guard.Argument(value).MinLength(3, customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal($"{customMessage} (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void ShouldThrows_CustomException_WithCustomMessage_When_ValueHasFewerCharactersAndCustomExceptionAndMessageWasUsed()
+    {
+        // Arrange
+        string value = "ab";
+        var customMessage = "Custom message.";
+
+        // Act
+
+        void Act()
+        {
+            ThrowHelper.Register((paramName, message) => new CustomException(paramName, message));
+            value = Guard.Argument(value).Throws<CustomException>().MinLength(3, customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<CustomException>(Act);
+        Assert.Equal(customMessage, ex.Message);
+    }
+    
+    class CustomException : Exception
+    {
+        public string ParamName { get; }
+
+        public CustomException(string paramName, string? message)
+            : base(message)
+        {
+            ParamName = paramName;
+        }
+    }
+}
---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.MinLength.cs
@@ -0,0 +1,14 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<string> MinLength(this ArgumentGuard<string> guard, uint minLength, string? message = null)
+    {
+        if (guard.Argument.Value.Length < minLength)
+        {
+            ThrowHelper.Throw<ArgumentException>(guard, message ?? $"The length of '{guard.Argument.Name}' must be {minLength} characters or more. Parameter {guard.Argument.Value.Length} has characters.");
+        }
+
+        return guard;
+    }
+}
