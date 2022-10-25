30b7a5bbb8b29603eec9be75a49b2453b312c756
Martin Dybal <martin@dybal.it>
2022-10-25 16:44:32 +0200

    ReSharper is trying to rename the other MultipleArgumentGuardExtensions files to MultipleArgumentGuardExtensions.cs. This file is here to prevent it from succeeding

---
+++ src/Utils/Dybal.Utils.Guards/MultipleArgumentGuardExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class MultipleArgumentGuardExtensions
+{
+    //ReSharper is trying to rename the other MultipleArgumentGuardExtensions files to MultipleArgumentGuardExtensions.cs. This file is here to prevent it from succeeding
+}
