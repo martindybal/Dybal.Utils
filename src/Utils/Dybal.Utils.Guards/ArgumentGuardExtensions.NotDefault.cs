a3e2393fbb73e0968a6c90cc00e0924762b01906
Martin Dybal <martin@dybal.it>
2022-10-26 22:43:04 +0200

    + HasValue Remove support from NUllable<T> from NotDefault.

---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.NotDefault.cs
@@ -0,0 +1,18 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<TArgument> NotDefault<TArgument>(this ArgumentGuard<TArgument> guard, string? message = null)
+        where TArgument : struct
+    {
+        if (EqualityComparer<TArgument>.Default.Equals(guard.Argument.Value, default))
+        {
+            var defaultMessage = guard.Argument.Value is Guid
+                ? "Value cannot be an empty GUID."
+                : "Value cannot be the default value.";
+            ThrowHelper.Throw<ArgumentException>(guard, message ?? defaultMessage);
+        }
+
+        return guard;
+    }
+}
