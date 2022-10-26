117b01c63ac0045451b161e6beffe3f8b970fd0f
Martin Dybal <martin@dybal.it>
2022-10-26 07:21:34 +0200

    + All

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/AllTests.cs
@@ -0,0 +1,49 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class AllTests : UnitTestsBase
+{
+    [Fact]
+    public void Should_NotThrows_When_AllItemsMatchPredicate()
+    {
+        // Arrange
+        var source = new[] { "  ", " " };
+
+        // Act
+        Guard.Argument(source).All(string.IsNullOrWhiteSpace);
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void Should_NotThrows_When_CollectionEmpty()
+    {
+        // Arrange
+        var source = Array.Empty<string>();
+
+        // Act
+        Guard.Argument(source).All(static _ => false);
+        
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void ShouldThrows_ArgumentException_When_CollectionNotContainPredicate()
+    {
+        // Arrange
+        var source = new[] { "a", "b" };
+
+        void Act()
+        {
+            Guard.Argument(source).All(string.IsNullOrWhiteSpace);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("All items of collection must match predicate. (Parameter 'source')", ex.Message);
+    }
+}
---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.All.cs
@@ -0,0 +1,23 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<IEnumerable<TArgument>> All<TArgument>(this ICovariantArgumentGuard<IEnumerable<TArgument>> covariantGuard, Func<TArgument, bool> filter, string? message = null)
+    {
+        var guard = ArgumentGuard<IEnumerable<TArgument>>.From(covariantGuard, covariantGuard.Argument);
+        return guard.All(filter, message);
+    }
+
+    public static ArgumentGuard<TEnumerable> All<TEnumerable, TArgument>(this ArgumentGuard<TEnumerable> guard, Func<TArgument, bool> filter, string? message = null)
+        where TEnumerable : IEnumerable<TArgument>
+    {
+        var source = guard.Argument.Value;
+        if (source.Any() && !source.All(filter))
+        {   
+            message ??= "All items of collection must match predicate.";
+            ThrowHelper.Throw<ArgumentException>(guard, message);
+        }
+
+        return guard;
+    }
+}
