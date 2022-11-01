176a058d5d8310082bc184f7c060b7e20b0ca72a
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2022-11-01 14:12:13 +0100

    Added initial infrastructure for API usage analyzers

---
+++ src/Analyzers/Dybal.Utils.Guards.Analyzers/GuardEqualsUsageAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Dybal.Utils.Guards.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class GuardEqualsUsageAnalyzer : ApiUsageAnalyzerBase
+{
+    public static DiagnosticDescriptor UseEqualInsteadOfEqualsRule = new DiagnosticDescriptor(
+#pragma warning disable RS2008 // Enable analyzer release tracking
+        id: DiagnosticIds.UseEqualToInsteadOfEquals,
+#pragma warning restore RS2008 // Enable analyzer release tracking
+        title: "Do not use invalid equality check",
+        messageFormat: @"When checking for equality, do not use ""Equals"" - use ""EqualTo"" instead",
+        DiagnosticCategories.ErrorProne,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    public GuardEqualsUsageAnalyzer()
+        : base(
+            descriptor: UseEqualInsteadOfEqualsRule,
+            typeFullName: "Dybal.Utils.Guards.ArgumentGuard`1",
+            methods: new[] { "Equals" })
+    {
+
+    }
+
+    protected override void AnalyzeInvocation(OperationAnalysisContext context, IInvocationOperation invocation, IMethodSymbol method)
+    {
+        // Whenever someone invokes .Equals on ArgumentGuard, issue a warning
+        var diagnostics = Diagnostic.Create(UseEqualInsteadOfEqualsRule, invocation.Syntax.GetLocation(), "Equals", "EqualTo");
+        context.ReportDiagnostic(diagnostics);
+    }
+}
---
+++ src/Analyzers/Dybal.Utils.Guards.Analyzers/Utilities/DiagnosticIds.cs
@@ -0,0 +1,9 @@
+﻿namespace Dybal.Utils.Guards.Analyzers;
+
+public static class DiagnosticIds
+{
+    // Reserved
+    public const string InternalAnalyzerError = "Guards01";
+
+    public const string UseEqualToInsteadOfEquals = "Guards02";
+}
