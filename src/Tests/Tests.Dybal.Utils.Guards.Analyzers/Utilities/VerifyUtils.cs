3fefffe3a4c3f0d0aeafd9afedea3cd23b71e882
Andrej Čižmárik <andrej.cizmarik@riganti.cz>
2022-11-01 14:13:17 +0100

    Added tests infrastructure for analyzer tests

---
+++ src/Tests/Tests.Dybal.Utils.Guards.Analyzers/Utilities/VerifyUtils.cs
@@ -0,0 +1,29 @@
+﻿using Dybal.Utils.Guards;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using VerifyCS = Tests.Dybal.Utils.Guards.Analyzers.CSharpAnalyzerVerifier<Dybal.Utils.Guards.Analyzers.GuardEqualsUsageAnalyzer>;
+
+namespace Tests.Dybal.Utils.Guards.Analyzers.Utilities
+{
+    public static class VerifyUtils
+    {
+        public static Task Test(string source, params DiagnosticResult[] diagnostics)
+        {
+            var test = new VerifyCS.Test()
+            {
+                TestCode = source,
+                TestState =
+                {
+                    ReferenceAssemblies = ReferenceAssemblies.Net.Net60,
+                    AdditionalReferences = { MetadataReference.CreateFromFile(typeof(Guard).Assembly.Location) }
+                }
+            };
+
+            foreach (var diagnostic in diagnostics)
+                test.ExpectedDiagnostics.Add(diagnostic);
+
+
+            return test.RunAsync();
+        }
+    }
+}
