c35f1ae24c7ec8085b7b427c9d5e33cd2bd952d3
Martin Dybal <martin@dybal.it>
2022-11-14 22:34:36 +0100

    TypedValue generated by roslyn source generator

---
+++ src/Tests/Tests.Dybal.Utils.TypedValues.SourceGenerators/Verifiers/CSharpSourceGeneratorVerifier.cs
@@ -0,0 +1,66 @@
+using System.Text;
+using Dybal.Utils.TypedValues;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Tests.Dybal.Utils.TypedValues.SourceGenerators.Verifiers
+{
+    internal static partial class CSharpSourceGeneratorVerifier<TSourceGenerator>
+        where TSourceGenerator : IIncrementalGenerator, new()
+    {
+        internal static readonly (string filename, string content)[] EmptyGeneratedSources = Array.Empty<(string filename, string content)>();
+
+        public static DiagnosticResult Diagnostic() => new();
+
+        public static DiagnosticResult Diagnostic(string id, DiagnosticSeverity severity) => new(id, severity);
+
+        public static DiagnosticResult Diagnostic(DiagnosticDescriptor descriptor) => new(descriptor);
+
+        public static async Task VerifyGeneratorAsync(string source, (string filename, string content) generatedSource)
+            => await VerifyGeneratorAsync(source, DiagnosticResult.EmptyDiagnosticResults, new[] { generatedSource });
+
+        public static async Task VerifyGeneratorAsync(string source, params (string filename, string content)[] generatedSources)
+            => await VerifyGeneratorAsync(source, DiagnosticResult.EmptyDiagnosticResults, generatedSources);
+
+        public static async Task VerifyGeneratorAsync(string source, DiagnosticResult diagnostic)
+            => await VerifyGeneratorAsync(source, new[] { diagnostic }, EmptyGeneratedSources);
+
+        public static async Task VerifyGeneratorAsync(string source, params DiagnosticResult[] diagnostics)
+            => await VerifyGeneratorAsync(source, diagnostics, EmptyGeneratedSources);
+
+        public static async Task VerifyGeneratorAsync(string source, DiagnosticResult diagnostic, (string filename, string content) generatedSource)
+            => await VerifyGeneratorAsync(source, new[] { diagnostic }, new[] { generatedSource });
+
+        public static async Task VerifyGeneratorAsync(string source, DiagnosticResult[] diagnostics, (string filename, string content) generatedSource)
+            => await VerifyGeneratorAsync(source, diagnostics, new[] { generatedSource });
+
+        public static async Task VerifyGeneratorAsync(string source, DiagnosticResult diagnostic, params (string filename, string content)[] generatedSources)
+            => await VerifyGeneratorAsync(source, new[] { diagnostic }, generatedSources);
+
+        public static async Task VerifyGeneratorAsync(string source, DiagnosticResult[] diagnostics, params (string filename, string content)[] generatedSources)
+        {
+            CSharpSourceGeneratorVerifier<TSourceGenerator>.Test test = new()
+            {
+                TestState =
+                {
+                    Sources = { source },
+                    AdditionalReferences = { (typeof(TypedValueAttribute).Assembly) },
+                },
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net60,
+                LanguageVersion = LanguageVersion.CSharp11,
+
+            };
+
+            foreach (var (filename, content) in generatedSources)
+            {
+                test.TestState.GeneratedSources.Add((typeof(TSourceGenerator), filename, SourceText.From(content, Encoding.UTF8)));
+            }
+
+            test.ExpectedDiagnostics.AddRange(diagnostics);
+
+            await test.RunAsync(CancellationToken.None);
+        }
+    }
+}
---
+++ src/Tests/Tests.Dybal.Utils.TypedValues/StringReadonlyTypedValueTests.cs
@@ -0,0 +1,26 @@
+﻿using Dybal.Utils.TypedValues;
+using Xunit;
+
+namespace Tests.Dybal.Utils.TypedValues;
+
+public class StringReadonlyTypedValueTests: StringReadonlyTypedValueTestsBase<TestTypedReadonlyString>
+{
+    [Fact]
+    public void Init_Throw_ArgumentException_when_ValidateValue_fails()
+    {
+        // Act
+        void Act()
+        {
+            var typedValue = CreateTypedValue("typed value");
+            var _ = typedValue with { Value = " " };
+        }
+
+        // Assert
+        Assert.Throws<ArgumentException>(Act);
+    }
+
+    protected override TestTypedReadonlyString CreateTypedValue(string value)
+    {
+        return new TestTypedReadonlyString(value);
+    }
+}
