59e5b462da54357d5c27d230c7d2bab2d0094c0e
Martin Dybal <martin@dybal.it>
2022-11-19 02:52:44 +0100

    + NotDefaultIfHasValue

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/NotDefaultIfHasValueTests.cs
@@ -0,0 +1,124 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class NotDefaultIfHasValueTests : UnitTestsBase
+{
+    [Fact]
+    public void NotThrows_When_null()
+    {
+        // Arrange
+        Guid? value = null;
+
+        // Act
+        Guard.Argument(value).NotDefaultIfHasValue();
+
+        // Assert
+        // doesn't throw any exception
+    }
+    
+    [Fact]
+    public void NotThrows_When_not_empty_guid()
+    {
+        // Arrange
+        Guid? value = Guid.NewGuid();
+
+        // Act
+        Guard.Argument(value).NotDefaultIfHasValue();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void NotThrows_When_not_default_int()
+    {
+        // Arrange
+        int? value = 1;
+
+        // Act
+        Guard.Argument(value).NotDefaultIfHasValue();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void Throw_ArgumentException_When_empty_guid()
+    {
+        // Arrange
+        Guid? defaultValue = Guid.Empty;
+
+        void Act()
+        {
+            Guard.Argument(defaultValue).NotDefaultIfHasValue();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be an empty GUID. (Parameter 'defaultValue')", ex.Message);
+    }
+
+    [Fact]
+    public void Throw_ArgumentException_When_default_int()
+    {
+        // Arrange
+        int? defaultValue = 0;
+
+        void Act()
+        {
+            Guard.Argument(defaultValue).NotDefaultIfHasValue();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value cannot be the default value. (Parameter 'defaultValue')", ex.Message);
+    }
+
+    [Fact]
+    public void Throw_ArgumentException_with_custom_message_When_was_used()
+    {
+        // Arrange
+        int? defaultValue = 0;
+        var customMessage = "Custom message.";
+
+        void Act()
+        {
+            Guard.Argument(defaultValue).NotDefaultIfHasValue(customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal($"{customMessage} (Parameter 'defaultValue')", ex.Message);
+    }
+
+    [Fact]
+    public void Throw_CustomException_When_Throws_was_used()
+    {
+        // Arrange
+        int? defaultValue = 0;
+
+        void Act()
+        {
+            ThrowHelper.TryRegister((paramName, message) => new CustomException(paramName, message));
+            Guard.Argument(defaultValue).Throws<CustomException>().NotDefaultIfHasValue();
+        }
+
+        // Assert
+        var customException = Assert.Throws<CustomException>(Act);
+        Assert.Equal(nameof(defaultValue), customException.ParamName);
+        Assert.Equal("Value cannot be the default value.", customException.Message);
+    }
+
+    class CustomException : Exception
+    {
+        public string ParamName { get; }
+
+        public CustomException(string paramName, string? message)
+            : base(message)
+        {
+            ParamName = paramName;
+        }
+    }
+}
