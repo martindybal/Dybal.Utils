f91fcbd2ce3678cc04b37ef3be9e575d950f42b1
Martin Dybal <martin@dybal.it>
2022-11-19 02:52:44 +0100

    + NotDefaultIfHasValue

---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.NotDefaultIfHasValue.cs
@@ -0,0 +1,16 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<TArgument?> NotDefaultIfHasValue<TArgument>(this ArgumentGuard<TArgument?> guard, string? message = null)
+        where TArgument : struct
+    {
+        if (guard.Argument.Value.HasValue)
+        {
+             var valueGuard = ArgumentGuard<TArgument>.From(guard, new Argument<TArgument>(guard.Argument.Value.Value, guard.Argument.Name));
+             valueGuard.NotDefault(message);
+        }
+
+        return guard;
+    }
+}
