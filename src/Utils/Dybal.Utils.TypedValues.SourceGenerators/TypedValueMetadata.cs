53b09c2553cfa4f4965ccb1287e8857a9d0118bc
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2022-11-22 10:51:46 +0100

    Support both generic and non-generic attributes (#9)
    * Update Roslyn to non-prerelease version
    
    * Support both generic and non-generic attributes

--- src/Utils/Dybal.Utils.TypedValues.SourceGenerators/TypedValueMetadata.cs
+++ src/Utils/Dybal.Utils.TypedValues.SourceGenerators/TypedValueMetadata.cs
@@ -2,27 +2,30 @@
 {
     public readonly record struct TypedValueMetadata
     {
-        public TypedValueMetadata(string name, string @namespace, Type valueType, string valueName, bool @readonly)
+        public TypedValueMetadata(string name, string @namespace, string valueType, string valueName, bool isValueReferenceType, bool @readonly)
         {
             this.Name = name;
             this.Namespace = @namespace;
             this.ValueType = valueType;
             this.ValueName = valueName;
+            this.IsValueReferenceType = isValueReferenceType;
             this.Readonly = @readonly;
         }
 
         public string Name { get; }
         public string Namespace { get; }
-        public Type ValueType { get; }
+        public string ValueType { get; }
         public string ValueName { get; }
+        public bool IsValueReferenceType { get; }
         public bool Readonly { get; }
 
-        public void Deconstruct(out string Name, out string Namespace, out Type ValueType, out string ValueName, out bool Readonly)
+        public void Deconstruct(out string Name, out string Namespace, out string ValueType, out string ValueName, out bool ReferenceType, out bool Readonly)
         {
             Name = this.Name;
             Namespace = this.Namespace;
             ValueType = this.ValueType;
             ValueName = this.ValueName;
+            ReferenceType = this.IsValueReferenceType;
             Readonly = this.Readonly;
         }
     }
