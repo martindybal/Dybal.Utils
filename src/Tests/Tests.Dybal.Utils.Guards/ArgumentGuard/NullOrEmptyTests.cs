52bbf131e01ee5718ffff531d514fb7d4c02d3e7
Martin Dybal <martin@dybal.it>
2023-11-27 15:07:34 +0100

    Add NullOrEmpty

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/NullOrEmptyTests.cs
@@ -0,0 +1,96 @@
+﻿using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class NullOrEmptyTests : UnitTestsBase
+{
+    [Fact]
+    public void NotThrow_When_null()
+    {
+        // Arrange
+        string? value = null;
+
+        // Act
+        Guard.Argument(value).NullOrEmpty();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void NotThrow_When_empty_string()
+    {
+        // Arrange
+        var value = string.Empty;
+
+        // Act
+        Guard.Argument(value).NullOrEmpty();
+
+        // Assert
+        // doesn't throw any exception
+    }
+
+    [Fact]
+    public void Throw_ArgumentException_When_value_is_not_null_or_empty()
+    {
+        // Arrange
+        string? value = "non-empty";
+
+        void Act()
+        {
+            Guard.Argument(value).NullOrEmpty();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value must be null or empty string. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void Throw_ArgumentException_with_custom_message_When_was_used()
+    {
+        // Arrange
+        string? value = "non-empty";
+        var customMessage = "Custom message.";
+
+        void Act()
+        {
+            value = Guard.Argument(value).NullOrEmpty(customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal($"{customMessage} (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void Throw_CustomException_When_Throws_was_used()
+    {
+        // Arrange
+        string? value = "non-empty";
+        var customMessage = "Custom message.";
+
+        void Act()
+        {
+            ThrowHelper.TryRegister((paramName, message) => new CustomException(paramName, message));
+            value = Guard.Argument(value).Throws<CustomException>().NullOrEmpty(customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<CustomException>(Act);
+        Assert.Equal(customMessage, ex.Message);
+        Assert.Equal(nameof(value), ex.ParamName);
+    }
+
+    class CustomException : Exception
+    {
+        public string ParamName { get; }
+
+        public CustomException(string paramName, string? message)
+            : base(message)
+        {
+            ParamName = paramName;
+        }
+    }
+}
---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.NullOrEmpty.cs
@@ -0,0 +1,15 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<string> NullOrEmpty(this ICovariantArgumentGuard<string?> covariantGuard, string? message = null)
+    {
+        var guard = ArgumentGuard<string>.From(covariantGuard, covariantGuard.Argument!);
+        if (!string.IsNullOrEmpty(guard.Argument.Value))
+        {
+            ThrowHelper.Throw<ArgumentException>(guard, message ?? "Value must be null or empty string.");
+        }
+
+        return guard;
+    }
+}
