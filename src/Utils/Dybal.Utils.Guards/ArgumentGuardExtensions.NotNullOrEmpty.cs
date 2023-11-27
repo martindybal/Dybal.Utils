e624cb63256ce50bbc0faea9fcaa459a22311569
Martin Dybal <martin@dybal.it>
2023-11-27 15:07:26 +0100

    Fix file name

---
+++ src/Utils/Dybal.Utils.Guards/ArgumentGuardExtensions.NotNullOrEmpty.cs
@@ -0,0 +1,15 @@
+﻿namespace Dybal.Utils.Guards;
+
+public static partial class ArgumentGuardExtensions
+{
+    public static ArgumentGuard<string> NotNullOrEmpty(this ICovariantArgumentGuard<string?> covariantGuard, string? message = null)
+    {
+        var guard = ArgumentGuard<string>.From(covariantGuard, covariantGuard.Argument!);
+        if (string.IsNullOrEmpty(guard.Argument.Value))
+        {
+            ThrowHelper.Throw<ArgumentException>(guard, message ?? "Value cannot be null or empty string.");
+        }
+
+        return guard;
+    }
+}
