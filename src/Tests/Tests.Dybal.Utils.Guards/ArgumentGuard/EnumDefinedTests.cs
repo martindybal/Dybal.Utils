adbdf3d2d245173e0b43de103a2fbd728c8e1973
Martin Dybal <martin.dybal@gmail.com>
2025-08-30 05:37:33 +0200

    feat: add enum defined guard

---
+++ src/Tests/Tests.Dybal.Utils.Guards/ArgumentGuard/EnumDefinedTests.cs
@@ -0,0 +1,89 @@
+using Dybal.Utils.Guards;
+using Xunit;
+
+namespace Tests.Dybal.Utils.Guards.ArgumentGuard;
+
+public class EnumDefinedTests : UnitTestsBase
+{
+    private enum SampleEnum
+    {
+        First = 1,
+        Second = 2,
+    }
+
+    [Fact]
+    public void NotThrow_When_value_is_defined()
+    {
+        // Arrange
+        SampleEnum value = SampleEnum.First;
+
+        // Act
+        SampleEnum guardValue = Guard.Argument(value).EnumDefined();
+
+        // Assert
+        Assert.Equal(value, guardValue);
+    }
+
+    [Fact]
+    public void Throw_ArgumentException_When_value_is_not_defined()
+    {
+        // Arrange
+        SampleEnum value = (SampleEnum)0;
+
+        void Act()
+        {
+            value = Guard.Argument(value).EnumDefined();
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal("Value of parameter 'value' (0) must be defined in enum 'SampleEnum'. (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void Throw_ArgumentException_with_custom_message_When_was_used()
+    {
+        // Arrange
+        SampleEnum value = (SampleEnum)0;
+        var customMessage = "Custom message.";
+
+        void Act()
+        {
+            value = Guard.Argument(value).EnumDefined(customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<ArgumentException>(Act);
+        Assert.Equal($"{customMessage} (Parameter 'value')", ex.Message);
+    }
+
+    [Fact]
+    public void Throw_CustomException_When_Throws_was_used()
+    {
+        // Arrange
+        SampleEnum value = (SampleEnum)0;
+        var customMessage = "Custom message.";
+
+        void Act()
+        {
+            ThrowHelper.TryRegister((paramName, message) => new CustomException(paramName, message));
+            value = Guard.Argument(value).Throws<CustomException>().EnumDefined(customMessage);
+        }
+
+        // Assert
+        var ex = Assert.Throws<CustomException>(Act);
+        Assert.Equal(customMessage, ex.Message);
+        Assert.Equal(nameof(value), ex.ParamName);
+    }
+
+    class CustomException : Exception
+    {
+        public string ParamName { get; }
+
+        public CustomException(string paramName, string? message)
+            : base(message)
+        {
+            ParamName = paramName;
+        }
+    }
+}
